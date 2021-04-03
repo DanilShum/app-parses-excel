737ae851ebf72870ddfcab8d6757d12c5b4f068b
DanilShum <danil.shumilkin.96@gmail.com>
2021-04-03 18:16:46 +0300

    add chart js

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <top-menu/>
+    <chart-page/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TopMenu from '@/components/TopMenu.vue'
+import ChartPage from '@/components/ChartPage.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
+    ChartPage,
     TopMenu
-  },
+  }
 })
 </script>
 <style lang="scss">
