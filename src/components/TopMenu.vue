57a43adcdce0fceb700a7333109269d49259b1ec
DanilShum <danil.shumilkin.96@gmail.com>
2021-04-03 11:09:58 +0300

    create base component

---
+++ src/components/TopMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <MegaMenu orientation="horizontal" class="top-menu">
+    <Button icon="pi pi-download" class="p-button-rounded p-button-success" />
+  </MegaMenu>
+  <chart-page/>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import Button from 'primevue/button'
+import MegaMenu from 'primevue/megamenu'
+import ChartPage from '@/components/ChartPage.vue'
+
+export default defineComponent({
+  name: 'TopMenu',
+  components: { ChartPage, Button, MegaMenu },
+  props: {
+
+  }
+})
+</script>
+
+<style lang="scss">
+.top-menu {
+
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,25 @@
 <template>
   <div class="home">
-
+    <top-menu/>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import TopMenu from '@/components/TopMenu.vue'
 
 export default defineComponent({
   name: 'Home',
   components: {
-
-  }
+    TopMenu
+  },
 })
 </script>
 <style lang="scss">
-
+  .home {
+    height: 100vh;
+    max-height: 100vh;
+    overflow: hidden;
+    max-width: 100%;
+  }
 </style>
